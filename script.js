e02b763c4d5492d6eed01037dbe4b146cdb913f8
ki3kia <31742915+ki3kia@users.noreply.github.com>
2022-04-10 09:18:32 +0000

    Deploying to gh-pages from @ ki3kia/Some-tasks@663bced47ca8388a633c3ba8e19b91fa19f52ddd 🚀

---
+++ script.js
@@ -0,0 +1,21 @@
+let section = document.createElement("section");
+let button = document.createElement("button");
+button.id = "addBlock";
+button.append("Add block");
+
+section.appendChild(button);
+document.body.prepend(section);
+
+button.onclick = addBlock;
+
+function addBlock(){
+    let arrSection = document.getElementsByTagName("section");
+    let divCont = arrSection[1];
+
+    let div = document.createElement("div");
+    const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+    div.style.backgroundColor = randomColor;
+    divCont.appendChild(div);
+    console.log(randomColor);
+}
