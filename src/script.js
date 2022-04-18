8605681a935de3fb510ef5c509e1117f2932c44a
Koндратюк Ірина <mandarinka.21.09.00@gmail.com>
2022-04-18 11:49:59 +0300

    events
    replase script to header and added events

--- src/script.js
+++ src/script.js
@@ -1,19 +1,22 @@
+
 let section = document.createElement("section");
 let button = document.createElement("button");
 button.id = "addBlock";
 button.append("Add block");
 
 section.appendChild(button);
-document.body.prepend(section);
+window.onload = function (){
+    document.body.prepend(section);
+}
 
-button.onclick = addBlock;
+button.addEventListener("click", addBlock);
 
-function addBlock(){
+function addBlock() {
     let arrSection = document.getElementsByTagName("section");
     let divCont = arrSection[1];
 
     let div = document.createElement("div");
-    const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
     div.style.backgroundColor = randomColor;
     divCont.appendChild(div);
